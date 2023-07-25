b6b6dfd4e608e9622ad15a38d9941107d6206422
MuhammadZain12 <uuu81201@gmail.com>
2023-07-25 21:29:33 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -3,60 +3,61 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
 
-
-app.get('/', (req, res) => {
-  res.send('Welcome From The Server');
-});
-
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.json({
     status: 'success',
-    results:tours.length,
+    results: tours.length,
     data: {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours',(req,res)=>{
-    const newId=tours.length+1;
-    const newTour=Object.assign({id:newId},req.body)
-    console.log(req.body)
-    tours.push(newTour)
-    fs.writeFile('./dev-data/data/tours.json',JSON.stringify(tours),err=>{
-        try{res.status(201).json(
-            {
-                status:"success",
-                data:{
-                    tour:newTour,
-                }
-            }
-        )}
-        catch{
-            res.end(err)
-        }
-    })
-})
+const addNewTour = (req, res) => {
+  const newId = tours.length + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  console.log(req.body);
+  tours.push(newTour);
+  fs.writeFile('./dev-data/data/tours.json', JSON.stringify(tours), (err) => {
+    try {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    } catch {
+      res.end(err);
+    }
+  });
+};
 
+const getSingleTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  console.log(id);
+  const tour = tours.find((item) => item.id === id);
+  console.log(tour);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-    console.log(req.params)
-    const id=req.params.id*1
-    console.log(id)
-    const tour=tours.find(item=>item.id===id)
-    console.log(tour)
-    res.status(200).json({
-        status:"success",
-        data:{
-            tour
-        }
-    })
-})
+// app.get('/api/v1/tours', getAllTours);
 
+// app.post('/api/v1/tours', addNewTour);
+
+// app.get('/api/v1/tours/:id', getSingleTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(addNewTour);
+app.route('/api/v1/tours/:id').get(getSingleTour)
 
 const port = 8000;
 app.listen(port, () => {
