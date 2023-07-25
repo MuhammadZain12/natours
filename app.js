5f69f6de408765efd51ef9f92d6f00c3ce6d84f1
MuhammadZain12 <uuu81201@gmail.com>
2023-07-25 21:09:30 +0500

    Done till patch request

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json())
 
-tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
 
 
 app.get('/', (req, res) => {
@@ -23,8 +23,40 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours',(req,res)=>{
+    const newId=tours.length+1;
+    const newTour=Object.assign({id:newId},req.body)
     console.log(req.body)
+    tours.push(newTour)
+    fs.writeFile('./dev-data/data/tours.json',JSON.stringify(tours),err=>{
+        try{res.status(201).json(
+            {
+                status:"success",
+                data:{
+                    tour:newTour,
+                }
+            }
+        )}
+        catch{
+            res.end(err)
+        }
+    })
 })
+
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params)
+    const id=req.params.id*1
+    console.log(id)
+    const tour=tours.find(item=>item.id===id)
+    console.log(tour)
+    res.status(200).json({
+        status:"success",
+        data:{
+            tour
+        }
+    })
+})
+
 
 const port = 8000;
 app.listen(port, () => {
