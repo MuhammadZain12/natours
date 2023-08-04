3a49870d5f241f70cf5d4d0b1d8e03c124dfa053
MuhammadZain12 <uuu81201@gmail.com>
2023-08-04 16:39:53 +0500

    Connected to Database and schema generated using mongoose

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const dotenv=require('dotenv')
-dotenv.config({path:'./config.env'})
-const app=require('./app')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
-if(process.env.NODE_ENV==='development'){
-  console.log(process.env)
-}
+const DB = process.env.DATABASE_CONNECTION.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+console.log('Connecting To Database');
+mongoose.connect(DB).then(() => console.log('Database connected'));
+
+const tourSchema = mongoose.Schema({
+  name: { type: String, required: [true, 'The name field is required'] },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
 
 const port = 8000;
 app.listen(port, () => {
