456a51e0b91c552408b0f8553e82895abc50df7e
MuhammadZain12 <uuu81201@gmail.com>
2023-08-31 23:46:45 +0500

    Global Error Handling Done For Url Not available on server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
+const AppError = require('./utils/appError');
+const gloablErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-console.log(app.get('env'))
+console.log(app.get('env'));
 
 app.use(express.json());
 
@@ -25,5 +27,12 @@ app.use('/api/v1/tours', tourRouter);
 
 // app.route('/api/v1/tours').get(getAllTours).post(addNewTour);
 // app.route('/api/v1/tours/:id').get(getSingleTour)
+
+// Error Handling
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(gloablErrorHandler);
 
 module.exports = app;
