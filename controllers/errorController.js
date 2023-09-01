46e6fa7325504f8e6a7a1d41c5c90ad228f8a01f
MuhammadZain12 <uuu81201@gmail.com>
2023-09-01 15:04:21 +0500

    Error seperated for Development and Production Environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,34 @@
+const sendErrorDev = (res, err) => {
+  res.status(err.statusCode).json({
+    err,
+    stack: err.stack,
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const sendErrorProd = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: 500,
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(res, err);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(res, err);
+  }
 };
