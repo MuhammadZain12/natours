a2dfbe864fbeefa086ce837e0a2df51d49cf27f1
MuhammadZain12 <uuu81201@gmail.com>
2023-09-05 20:50:27 +0500

    user-model added

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A name is required for user'],
+    maxlength: [40, 'Name must not be greater than 40 characters'],
+    minlength: [5, 'Name must be greater than 5 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail],
+  },
+  photo: { type: String },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    minlength: [8, 'Password should be greater than 8 characters.'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password.'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
