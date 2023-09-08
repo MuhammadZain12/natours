8a70acaf9f07a221e3f92414c716e0aa791c9ddb
MuhammadZain12 <uuu81201@gmail.com>
2023-09-08 23:13:57 +0500

    Signup functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  };
+  const newUser = await User.create(newUserData);
+
+  token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRETE, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
